4281d0fa5ca4f246d0cfb47a71fbd351cbf86b11
florent-dartora-tamu <157429027+florent-dartora-tamu@users.noreply.github.com>
2024-02-07 12:17:36 -0600

    Update ArcPy_Script.py

--- Lab/Lab07/ArcPy_Script.py
+++ Lab/Lab07/ArcPy_Script.py
@@ -7,18 +7,18 @@ band2 = arcpy.sa.Raster(source + r"\Band2.TIF") # green
 band3 = arcpy.sa.Raster(source + r"\Band3.TIF") # red
 band4 = arcpy.sa.Raster(source + r"\Band4.TIF") # NIR
 
-composite = arcpy.CompositeBands_management([band1, band2, band3, band4], source + r"\combined.tif")
+composite = arcpy.CompositeBands_management([band1, band2, band3, band4], source + r"\outputCombined.tif")
 
 # Hillshade
 azimuth = 315
 altitude = 45
 shadows = "NO_SHADOWS"
 z_factor = 1
-arcpy.ddd.HillShade(source + r"\dem.hgt", source + r"\hillshade.tif", azimuth, altitude, shadows, z_factor)
+arcpy.ddd.HillShade(source + r"\dem.tif", source + r"\outputHillshade.tif", azimuth, altitude, shadows, z_factor)
 
 # Slope
 output_measurement = "DEGREE"
 z_factor = 1
-arcpy.ddd.Slope(source + r"\dem.hgt", source + r"\slopes.tif",output_measurement, z_factor)
+arcpy.ddd.Slope(source + r"\dem.tif", source + r"\outputSlopes.tif",output_measurement, z_factor)
 
 print("success")
